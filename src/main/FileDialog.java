495fedeb34227a05f611377d93cc6cf6f645de81
Garrett Hart <garrett7621@gmail.com>
2017-02-25 17:44:57 -0500

    beginning of file browser
    can select a file with a directory but would now like to be able to have
    a type of windows file explore in a JPanel

---
+++ src/main/FileDialog.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class FileDialog implements ActionListener{
+	Object button;
+	JFrame frame;
+	public FileDialog(JFrame frame, Object button){
+		this.button = button;
+		this.frame = frame;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 int result;
+	        
+		    JFileChooser chooser = new JFileChooser(); 
+		    chooser.setCurrentDirectory(new java.io.File("."));
+		    chooser.setDialogTitle("Title");
+		    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		    //
+		    // disable the "All files" option.
+		    //
+		    chooser.setAcceptAllFileFilterUsed(false);
+		    //    
+		    if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) { 
+		      System.out.println("getCurrentDirectory(): " 
+		         +  chooser.getCurrentDirectory());
+		      System.out.println("getSelectedFile() : " 
+		         +  chooser.getSelectedFile());
+		      }
+		    else {
+		      System.out.println("No Selection ");
+		      }
+		     }
+		   
+
+
+}
--- src/main/MainForm.java
+++ src/main/MainForm.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,6 +10,8 @@ import javax.swing.JFrame;
 public class MainForm extends JFrame implements ActionListener{
 	JFrame mainFrame = new JFrame();
 	JButton exitButton = new JButton();
+	JButton fileButton = new JButton();
+	FileDialog fileChooser = new FileDialog(mainFrame, fileButton);
 	
 	public MainForm(){
 		setup();
@@ -16,15 +19,32 @@ public class MainForm extends JFrame implements ActionListener{
 	
 	public void setup(){
 		
+		mainFrame.setTitle("DataSync");
+		mainFrame.setSize(800, 550);
+		exitButton.setText("Exit");
+		exitButton.setSize(60, 25);
+		fileButton.setText("Choose Directory");
+		fileButton.setSize(150, 25);
+		mainFrame.add(exitButton);
+		exitButton.setLocation(0, mainFrame.getHeight() - 53);
+		fileButton.setLocation(mainFrame.getWidth() - 650, mainFrame.getHeight() - 450);
+		mainFrame.add(fileButton);
+		mainFrame.setLayout(null);
+		exitButton.addActionListener(this);
+		fileButton.addActionListener(fileChooser);
+		mainFrame.setVisible(true);
+		mainFrame.setResizable(false);
 	}
+	
+	
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		Object source = e.getSource();
-		if (true){
-			
+		if (source == exitButton){
+			System.exit(0);
 		}
+		
 	}
 
 
